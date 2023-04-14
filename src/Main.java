0d93a22adac181a7f93dedfaf3bd15c2154ec949
HERRINGCW22 <herringcw22@gcc.edu>
2023-04-14 12:35:17 -0400

    i did nothing helpful... tried to fix vigenere class, got nowhere..

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main {
         CompositionCipher comp = new CompositionCipher();
         System.out.println(comp.encrypt('b'));
         System.out.println(comp.decrypt('d'));
-        comp.add(new CaesarCipher(19));
-        comp.add(new CaesarCipher(12));
+
         System.out.println(comp.encrypt('a'));
 
     }
